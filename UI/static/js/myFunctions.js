995f3aa65355c59ae76f9b43c37a673cdd3d0945
Paul Balitema <engineerbpk@gmail.com>
2018-10-15 19:56:43 +0300

    feat-profile_page-161223570
    Designed profile page for admin and user where a quick glance at
    some information can be done or proceed to app details through the
    menu.
    
    [DELIVERS #161223570, #161134450, #161133690]

--- UI/static/js/myFunctions.js
+++ UI/static/js/myFunctions.js
@@ -3,11 +3,11 @@ function loginCheckBox(){
 
     var x = document.getElementsByTagName("form")[0].getAttribute("action");
     
-    if (x == "inventory.html"){
-        document.getElementsByTagName("form")[0].setAttribute("action", "admin_inventory.html");
+    if (x == "profile.html"){
+        document.getElementsByTagName("form")[0].setAttribute("action", "admin_profile.html");
     }
     else{
-        document.getElementsByTagName("form")[0].setAttribute("action", "inventory.html");
+        document.getElementsByTagName("form")[0].setAttribute("action", "profile.html");
     }
    
 }
