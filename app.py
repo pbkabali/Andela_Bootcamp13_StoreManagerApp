3390539a545e9201a72b50403b597efcfbefe95e
Paul Balitema <engineerbpk@gmail.com>
2018-10-19 02:22:01 +0300

    feat-sales_records-161271636
    added endpoint to create sales record. one endpoint to ad products to
    cart and the other to create the sale record.
    
    [DELIVERS #161271636, #161271693]

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import store_manager
 
 app = Flask(__name__)
 
-action = store_manager.Product_Operation()
+action = store_manager.Product_Operation("Yivanna Biles")
 
 @app.route('/')
 def home_page():
@@ -17,8 +17,9 @@ def create_product():
         unit_price = int(request.form.get('unit_price'))
         quantity = int(request.form.get('quantity'))
         minimum_quantity = int(request.form.get('minimum_quantity'))      
-        action.admin_create_product(name, unit, unit_price, quantity, minimum_quantity)       
-        return jsonify(store_manager.inventory)
+        action.admin_create_product(name, unit, unit_price, quantity, minimum_quantity)  
+        return action.response
+        # return jsonify(store_manager.inventory)
 
 @app.route('/v1/products/<int:productId>')
 def get_product_by_id(productId):    
@@ -28,6 +29,27 @@ def get_product_by_id(productId):
 @app.route('/v1/products')
 def get_all_products():
     return jsonify(store_manager.inventory)
+
+@app.route('/v1/sales/shopping_cart', methods = ['GET', 'POST'])
+def shopping_cart():
+    if request.method == 'POST':
+        product_id = int(request.form.get('product_id'))
+        quantity = int(request.form.get('quantity'))
+        action.add_to_cart_by_id(product_id,quantity)
+        return action.response       
+
+    return jsonify(action.shopping_cart) 
+
+@app.route('/v1/sales/create_record')
+def create_sales_record():
+    action.create_sales_record()
+    return action.response
+   
+
+@app.route('/v1/sales/get_all_records')
+def get_all_records():
+    return jsonify(store_manager.sales_records)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- store_manager.py
+++ store_manager.py
@@ -1,41 +1,116 @@
-inventory = [] #list of all added products + details 
+from datetime import datetime
+
+inventory = {   #dictionary of all added products + details 
+    1: {
+        "product_name": "baseball_hats"
+        ,"unit_of_measure": "pcs"
+        ,"unit_price": 10
+        ,"quantity": 35
+        ,"minumum_quantity":2
+    } 
+    
+    ,2: {
+        "product_name": "blue_jeans"
+        ,"unit_of_measure": "pairs"
+        ,"unit_price": 25
+        ,"quantity": 80
+        ,"minumum_quantity":5
+    } 
+    ,3: {
+        "product_name": "mens_vests"
+        ,"unit_of_measure": "pcs"
+        ,"unit_price": 8
+        ,"quantity": 54
+        ,"minumum_quantity":10
+    } 
+}  
+"""dictionary of sales records from all attendants"""
+sales_records = {} 
 
 class Product_Operation:
+
+    def __init__(self, requester):              
+        self.requester = requester
+        self.shopping_cart = {}
+        self.total_price_in_cart = 0
+        self.total_items_in_cart = 0
 
     def admin_create_product(self, name, unit_of_measure, unit_price 
                             ,initial_quantity, minimum_quantity):
 
         """This method will add a dictionary of the  product details 
-        to the inventory list after checking that this product is not 
+        to the inventory  after checking that this product is not 
         already in the inventory list.
         """
         product_duplicate = False #set variable that will be toggled incase a duplicate is found
         if len(inventory) > 0:              
             for product in inventory:                
-                if product["product_name"] == name:
+                if inventory[product]["product_name"] == name:
                     product_duplicate = True   
 
         if product_duplicate:                    
-            self.response = "Product already exists!"
+            self.response = "Product already exists! Choose edit instead"
             return False   
         else:  
-            inventory.append({"product_id":len(inventory)+1
-                        ,"product_name":name
+            inventory[len(inventory)+1] = {
+                        "product_name":name
                         ,"unit_of_measure":unit_of_measure
                         ,"unit_price":unit_price
                         ,"quantity":initial_quantity
                         ,"minumum_quantity":minimum_quantity
-                        })      
+                        }      
             self.response = "Product added to inventory successfully!"
             return True                   
         
     def get_product_by_id(self,product_id):
         """method to get back dictionary of product details from inventory
-        list when the user supplies the product id.
+        dictionary when the user supplies the product id.
         """
-        for product in inventory:
-            if product["product_id"] == product_id:
-                return product    
+        return inventory[product_id]  
+
+
+    def add_to_cart_by_id(self, product_id, quantity):
+
+        if product_id > len(inventory):
+            self.response = "Product not found!"
+
+        elif inventory[product_id]["quantity"] < quantity:
+            self.response = "Not enough quantity in inventory!"
+
+        else: 
+            if len(self.shopping_cart) == 0:
+                id_in_cart = 1
+            else:    
+                id_in_cart = len(self.shopping_cart)-1 # subtract the two addional rows of 
+                                                        #total items and total price         
+            self.shopping_cart[str(id_in_cart)] = {
+                "product_name":inventory[product_id]["product_name"]
+                ,"quantity_ordered":quantity
+                ,"price": quantity*inventory[product_id]["unit_price"]
+            }
+            self.total_price_in_cart += quantity*inventory[product_id]["unit_price"]
+            self.total_items_in_cart += quantity
+            self.shopping_cart["total_price"] = self.total_price_in_cart
+            self.shopping_cart["total_items_in_cart"] = self.total_items_in_cart
+
+            inventory[product_id]["quantity"] -= quantity
+
+            self.response = "Product added to cart successfully!"
+
+    def create_sales_record(self):
+            self.shopping_cart["created-by:"] = self.requester
+            self.shopping_cart["created_at:"] = datetime.now()            
+            sales_records[len(sales_records)+1] = self.shopping_cart
+            # self.shopping_cart.clear() - check ehy this clears all sales records
+            self.response = "Sales Record Created Successfully"
+
+            
+
+
+
+
+
+
                 
       
 
