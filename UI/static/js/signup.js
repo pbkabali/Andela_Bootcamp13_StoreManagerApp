518761f5b923e5c5d4e9f8970e6f2bee6f2a1229
Paul Balitema <engineerbpk@gmail.com>
2018-11-28 13:31:32 +0300

    add password confirmation on sign-up
    add input field to retype password at signup
    
    [DELIVERS #162155384, #162160352]

--- UI/static/js/signup.js
+++ UI/static/js/signup.js
@@ -6,32 +6,39 @@ submit.addEventListener("click", registerUser);
 function registerUser(){
     let firstName = document.getElementById('first-name').value,
         lastName = document.getElementById('last-name').value,
-        username = document.getElementById('username').value,    
-        password = document.getElementById('password').value;
+        username = document.getElementById('username').value,
+        password = document.getElementById('password').value,    
+        confirmPassword = document.getElementById('confirm-password').value;
+
+    if (password == confirmPassword){    
     
     
-    fetch(url, {
-        method: "POST",
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({
-            first_name: firstName,
-            last_name : lastName,
-            username : username,
-            password : password
+        fetch(url, {
+            method: "POST",
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                first_name: firstName,
+                last_name : lastName,
+                username : username,
+                password : password
+            })
         })
-    })
-    .then((res) => res.json())
-    .then(data => {
-        if (data.access_token){ 
-            alert(data.Response +"\n Please login with your details");           
-            redirect: window.location.replace("index.html");
-            // localStorage.setItem('token', data.access_token)
-        }
-        else{
-            alert(data.response);
-            console.log(data);
-        }
-    })   
+        .then((res) => res.json())
+        .then(data => {
+            if (data.access_token){ 
+                alert(data.Response +"\n Please login with your details");           
+                redirect: window.location.replace("index.html");
+                // localStorage.setItem('token', data.access_token)
+            }
+            else{
+                alert(data.response);
+                console.log(data);
+            }
+        })
+    }   
+    else{
+        alert("Passwords do not match!");
+    }
 }
 
 
