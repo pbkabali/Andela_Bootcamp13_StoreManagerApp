625c0e5bd2ce109e0ce926c72cbe623411e2760a
Paul Balitema <engineerbpk@gmail.com>
2018-12-02 03:02:29 +0300

    ft-fetch-byId
    implement fetching single product and single sale record by Id
    
    [DELIVERS #162160496, #162160387]

--- UI/static/js/admin_all_sales.js
+++ UI/static/js/admin_all_sales.js
@@ -1,5 +1,6 @@
+const all_sales_table = document.getElementById('all-sales-table');
 window.onload = function() {
-    const all_sales_table = document.getElementById('all-sales-table'),
+    const 
         all_sales_url = 'https://polos-store-manager.herokuapp.com/api/v1/admin/sales/all';
 
     fetch(all_sales_url, {
@@ -7,7 +8,10 @@ window.onload = function() {
     })
     .then((res) => res.json())
     .then((data) => {
-        let table_contents = `
+        let total_products = 0,
+            total_records = 0,
+            total_revenue = 0,
+            table_contents = `
         <tr>
             <th>ID</th>
             <th>Date of Sale</th>
@@ -18,19 +22,96 @@ window.onload = function() {
         </tr>
         `;
         data.sales_records.forEach(record => {
+            total_products += record.quantity;
+            total_records++;
+            total_revenue += record.total_price;
             table_contents +=`
-            <tr>
-                <td id = "date-of-sale">${record.sale_id}</td>
-                <td id = "date-of-sale">${record.created_at}</td>
-                <td id = "id-number">${record.attendant_id}</td>  
-                <td id = "product-name">${record.product_name}</td>    
-                <td id = "quantity">${record.quantity}</td> 
-                <td id = "amount">${record.total_price}</td>                                                                      
+            <tr onclick="recordDetails(this)" data-id ="${record.sale_id}"> 
+                <td onclick="tag()">${record.sale_id}</td>
+                <td>${record.created_at}</td>
+                <td>${record.attendant_id}</td>  
+                <td>${record.product_name}</td>    
+                <td>${record.quantity}</td> 
+                <td>${record.total_price}</td> 
+
             </tr>     
-            `;            
+            `;              
         });
-        all_sales_table.innerHTML = table_contents;
+        all_sales_table.innerHTML = table_contents; 
+        document.getElementById('total-records').innerHTML = total_records; 
+        document.getElementById('total-products').innerHTML = total_products; 
+        document.getElementById('total-revenue').innerHTML = total_revenue;       
     })
-  }
+  }  
+
+
+function recordDetails(e){   
+    let saleId = e.getAttribute('data-id'),
+        single_sale_url = 'https://polos-store-manager.herokuapp.com/api/v1/sales/'+saleId;
+
+    fetch(single_sale_url, {
+        headers: {'Authorization': 'Bearer '+ localStorage.getItem('token')}
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            let sale_details = `
+            <h2 class = "form-heading">DETAILS OF SALE</h2>
+            <br>
+            <table class = "table1">                                                        
+                    
+                <tr>
+                    <th>Detail</th>
+                    <th>Value</th>                           
+                </tr>
+                <tr>               
+                    <td>Sale-ID</td>
+                    <td>${data.found_record.sale_id}</td>                 
+                </tr>     
+                <tr>               
+                    <td>Sale Date</td>
+                    <td>${data.found_record.created_at}</td>                 
+                </tr>   
+                <tr>               
+                    <td>Product ID</td>
+                    <td>${data.found_record.product_id}</td>                 
+                </tr>     
+                <tr>               
+                    <td>Product Name</td>
+                    <td>${data.found_record.product_name}</td>                 
+                </tr>   
+                <tr>               
+                    <td>Quantity Sold</td>
+                    <td>${data.found_record.quantity}</td>                 
+                </tr>  
+                <tr>               
+                    <td>Unit Price</td>
+                    <td>${data.found_record.unit_price}</td>                 
+                </tr>  
+                <tr>               
+                    <td>Total Price</td>
+                    <td>${data.found_record.total_price}</td>                 
+                </tr>   
+                <tr>               
+                    <td>Attendant-ID</td>
+                    <td>${data.found_record.attendant_id}</td>                 
+                </tr>
+                <tr>               
+                    <td>Attendant's Name</td>
+                    <td>Name</td>                 
+                </tr>
+            </table> 
+            <br>
+            <br>
+            <a href = "admin_overall_sales.html">Back to All sales</a>
+            <br>
+            <br>                     
+            `;
+            document.getElementById('form-div').innerHTML = sale_details;            
+        })  
+}
+
+
+
+
 
 
--- UI/static/js/admin_inventory.js
+++ UI/static/js/admin_inventory.js
@@ -14,23 +14,75 @@ window.onload = function() {
         <th>Product Name</th>
         <th>Quantity in Stock</th>
         <th>Unit Price</th>
-        <th>Edit Product</th>
+       
         </tr>
         `;
         data.available_products.forEach(product => {
             table_contents +=`
-            <tr> 
+            <tr data-id = "${product.product_id}" onclick = "productDetails(this)"> 
                 <td id = "product-category">${product.product_id}</td>                               
                 <td id = "product-category">${product.category}</td>
                 <td id = "item-name">${product.product_name}</td>
                 <td id = "item-quantity">${product.quantity}</td>
                 <td id = "item-unitprice">${product.unit_price}</td>
-                <td><button class= "button3">Edit/Delete</button></td>                           
+                                       
             </tr>
             `;            
         });
         products_table.innerHTML = table_contents;
     })
   }
+  function productDetails(e){   
+    let productId = e.getAttribute('data-id'),
+        single_product_url = 'https://polos-store-manager.herokuapp.com/api/v1/products/'+productId;
 
+    fetch(single_product_url, {
+        headers: {'Authorization': 'Bearer '+ localStorage.getItem('token')}
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            let product_details = `
+            <h2 class = "form-heading">PRODUCT DETAILS</h2>
+            <br>
+            <table class = "table1">                                                        
+                    
+                <tr>
+                    <th>Detail</th>
+                    <th>Value</th>                           
+                </tr>
+                <tr>               
+                    <td>product ID</td>
+                    <td>${data.found_product.product_id}</td>                 
+                </tr>    
+                <tr>               
+                    <td>Product Name</td>
+                    <td>${data.found_product.product_name}</td>                 
+                </tr>   
+                <tr>               
+                    <td>Quantity in Stock</td>
+                    <td>${data.found_product.quantity}</td>                 
+                </tr>  
+                <tr>               
+                    <td>Unit Price</td>
+                    <td>${data.found_product.unit_price}</td>                 
+                </tr>  
+                <tr>               
+                    <td>Unit of measure</td>
+                    <td>${data.found_product.unit}</td>                 
+                </tr>   
+                <tr>               
+                    <td>Minimum Allowed quantity</td>
+                    <td>${data.found_product.minimum_quantity}</td>                 
+                </tr>
+            </table> 
+            <br>
+            <br>
+            <a href = "admin_inventory.html">Back to All products</a>
+            <br>
+            <br>                     
+            `;
+            document.getElementById('form-div').innerHTML = product_details;
+           
+        })  
+}
 
